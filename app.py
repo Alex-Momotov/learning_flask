38e55e2e59f54a7e6824aaac261cedb9b3f3a89a
Alex Momotov <alex.momotov@vonage.com>
2019-11-09 20:33:40 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '18766532af05576ba1b839e53adc7e2a'
+
+posts = [
+    {
+        'author':'Corey Schafer',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author':'Jane Doe',
+        'title':'Blog post 2',
+        'content':'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+
+]
+
+
+@app.route('/home')
+@app.route('/')
+def home():
+    return render_template('home.html', posts = posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
